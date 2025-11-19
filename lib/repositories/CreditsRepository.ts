812b79cb65d72351b4994fcf3dacaaecaccbbb26
MaydayTM <mehdi.held@icloud.com>
2025-11-19 11:52:04 +0100

    fix: split token generation into separate variables

--- lib/repositories/CreditsRepository.ts
+++ lib/repositories/CreditsRepository.ts
@@ -24,7 +24,9 @@ export class CreditsRepository {
     }
 
     // Generate new session token
-    const newToken = `sess_${Date.now()}_${Math.random().toString(36).substring(2, 15)}`
+    const timestamp = Date.now()
+    const randomStr = Math.random().toString(36).substring(2, 15)
+    const newToken = `sess_${timestamp}_${randomStr}`
 
     // Set cookie (expires in 30 days)
     (await cookies()).set(CREDIT_SESSION_COOKIE, newToken, {
